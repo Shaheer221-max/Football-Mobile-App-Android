0159e2cc3ab4d27664ec0fac21235fefb208733f
Shaheer221-max <55945331+Shaheer221-max@users.noreply.github.com>
2023-06-02 07:35:28 +0500

    first commit

---
+++ Screens/Port/Port.js
@@ -0,0 +1,7 @@
+const ports = {
+  herokuPort: 'http://ec2-16-171-46-174.eu-north-1.compute.amazonaws.com:5000',
+  // herokuPort: 'http://192.168.18.5:3000',
+  // socketPort: 'http://192.168.18.5:4000',
+  socketPort: 'http://ec2-16-171-46-174.eu-north-1.compute.amazonaws.com:5000',
+};
+export default ports;
